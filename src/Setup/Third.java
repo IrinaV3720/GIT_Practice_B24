7e64a0a536027c10a9be67f5b799b4b18a5bf9f8
IrinaV3720 <iraalazarenko@gmail.com>
2021-10-03 11:46:46 -0500

    Added additional information to a 3rd file twice

--- src/Setup/Third.java
+++ src/Setup/Third.java
@@ -4,6 +4,6 @@ public class Third {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Irina");
-        System.out.println("I am learning Java");
+        System.out.println("I am learning Java and it is interesting");
     }
 }
